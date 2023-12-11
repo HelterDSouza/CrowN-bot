31c38252fcd56280fbacab71abf336ca98134e1a
Helter Daniel <helterdaniel@hotmail.com>
2023-12-10 22:41:16 -0300

    🚧 add tracing loggers

--- src/main.rs
+++ src/main.rs
@@ -9,7 +9,10 @@ use sqlx::{
     sqlite::{SqliteConnectOptions, SqlitePoolOptions},
     Pool, Sqlite,
 };
+use tracing_subscriber::{layer::SubscriberExt, util::SubscriberInitExt};
+
 use crate::config::config;
+
 mod config;
 async fn initialize_framework() -> StandardFramework {
     let framework = StandardFramework::new();
@@ -20,12 +23,16 @@ async fn initialize_intents() -> GatewayIntents {
     GatewayIntents::non_privileged() | GatewayIntents::MESSAGE_CONTENT
 }
 async fn run_migration(pool: &Pool<Sqlite>) {
+    tracing::info!("Initiating database migration process");
     sqlx::migrate!("./migrations")
         .run(pool)
         .await
         .expect("Error Migration");
+    tracing::info!("Database migration process completed successfully");
 }
 async fn initialize_database(config: &Config) -> Result<Pool<Sqlite>> {
+    tracing::info!("Database initialization in progress");
+    tracing::debug!("{}", config.db_url());
 
     let pool = SqlitePoolOptions::new()
         .max_connections(5)
@@ -33,13 +40,27 @@ async fn initialize_database(config: &Config) -> Result<Pool<Sqlite>> {
         .await?;
 
     run_migration(&pool).await;
+    tracing::info!("Database initialization completed successfully");
 
     Ok(pool)
 }
+
+fn init_tracing() {
+    tracing_subscriber::registry()
+        .with(
+            tracing_subscriber::EnvFilter::try_from_default_env()
+                // .unwrap_or_else(|_| "crown_bot=info".into()),
+                .unwrap_or_else(|_| "info".into()),
+        )
+        .with(tracing_subscriber::fmt::layer())
+        .init();
+}
+
 #[tokio::main]
 async fn main() -> Result<()> {
     let config = config().await;
 
+    init_tracing();
 
     let fw = initialize_framework().await;
 
